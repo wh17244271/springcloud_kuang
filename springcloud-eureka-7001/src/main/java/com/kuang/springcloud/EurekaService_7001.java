e3291d5048b9f9b3c1a90fbe81c54bd4a88cfcd3
wh17244271 <290827944@qq.com>
2020-07-16 16:19:42 +0800

    sdf

--- springcloud-eureka-7001/src/main/java/com/kuang/springcloud/EurekaService_7001.java
+++ springcloud-eureka-7001/src/main/java/com/kuang/springcloud/EurekaService_7001.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaService_7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaService_7001.class,args);
+
     }
 }
