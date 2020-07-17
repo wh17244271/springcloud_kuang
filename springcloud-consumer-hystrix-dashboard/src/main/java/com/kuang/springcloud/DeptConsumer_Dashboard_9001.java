121c7164d4deec1e97e362513f5b3166bd04b7cd
wh17244271 <290827944@qq.com>
2020-07-17 17:02:13 +0800

    服务熔断和降级

--- springcloud-consumer-hystrix-dashboard/src/main/java/com/kuang/springcloud/DeptConsumer_Dashboard_9001.java
+++ springcloud-consumer-hystrix-dashboard/src/main/java/com/kuang/springcloud/DeptConsumer_Dashboard_9001.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+/**
+ * 监控的服务必须配置固定写法  案例 (springcloud-provider-dept-hystrix-8001 包中的 启动类 有例子 )
+ */
 @SpringBootApplication
 @EnableHystrixDashboard //开启监控
 public class DeptConsumer_Dashboard_9001 {
