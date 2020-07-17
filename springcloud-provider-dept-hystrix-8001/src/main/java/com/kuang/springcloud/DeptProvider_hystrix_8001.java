776bb8457e13fc79e0cef25c7438f82cf81b5a80
wh17244271 <290827944@qq.com>
2020-07-17 17:01:09 +0800

    服务熔断和降级

--- springcloud-provider-dept-hystrix-8001/src/main/java/com/kuang/springcloud/DeptProvider_hystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/kuang/springcloud/DeptProvider_hystrix_8001.java
@@ -7,14 +7,12 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient  //服务发现
 @EnableCircuitBreaker  //开启断路器
-@EnableHystrix
 public class DeptProvider_hystrix_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_hystrix_8001.class,args);
