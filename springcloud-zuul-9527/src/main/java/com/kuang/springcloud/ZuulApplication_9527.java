b67d7065a10eccbb095181c5c10a16f02f51ef40
wh17244271 <290827944@qq.com>
2020-07-20 09:21:30 +0800

    zuul网关

---
+++ springcloud-zuul-9527/src/main/java/com/kuang/springcloud/ZuulApplication_9527.java
@@ -0,0 +1,16 @@
+package com.kuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy //开启zuul代理
+public class ZuulApplication_9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication_9527.class,args);
+    }
+
+    //http://www.kuangstudy.com:9527/springcloud-provider-dept/dept/get/1  参考网关地址
+
+}
